666348db94d8dec490682c36d0fe31e6af7d89fc
Shivani Chavan <76966366+Shivani31996@users.noreply.github.com>
2022-05-26 02:01:47 -0400

    Time: 36 ms (39.04%), Space: 63.5 MB (31.70%) - LeetHub

---
+++ 705-design-hashset/705-design-hashset.java
@@ -0,0 +1,65 @@
+class MyHashSet {
+    
+    private boolean [][] storage;
+    private int buckets;
+    private int bucketItems;
+
+    public MyHashSet() {
+        buckets = 1000;
+        bucketItems = 1000;
+        storage = new boolean[buckets][];
+    }
+    
+    private int bucketHashing(int key){
+        return key % buckets;
+    }
+    
+    private int bucketItemsHashing(int key){
+        return key / bucketItems;
+    }
+    
+    public void add(int key) {
+        int bucket = bucketHashing(key);
+        int bucketItem = bucketItemsHashing(key);
+        
+        if(storage[bucket] == null){
+            if(bucket == 0)
+            {
+                storage[bucket] = new boolean[bucketItems + 1];
+            }            
+        else{
+            storage[bucket] = new boolean[bucketItems];
+        }
+    }
+        storage[bucket][bucketItem] = true;
+    }
+    
+    public void remove(int key) {
+        int bucket = bucketHashing(key);
+        int bucketItem = bucketItemsHashing(key);
+        
+        if(storage[bucket] == null)
+        {
+            return;
+        }
+        else{
+            storage[bucket][bucketItem] = false;
+        }        
+    }
+    
+    public boolean contains(int key) {
+        int bucket = bucketHashing(key);
+        int bucketItem = bucketItemsHashing(key);
+        if(storage[bucket] == null)
+            return false;
+        return storage[bucket][bucketItem];
+    }
+}
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet obj = new MyHashSet();
+ * obj.add(key);
+ * obj.remove(key);
+ * boolean param_3 = obj.contains(key);
+ */
