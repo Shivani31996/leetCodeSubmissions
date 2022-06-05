dc618d451e44351e4861dcb60eb9aba556475b11
Shivani Chavan <76966366+Shivani31996@users.noreply.github.com>
2022-06-05 19:04:53 -0400

    Time: 30 ms (45.27%), Space: 57.2 MB (15.43%) - LeetHub

--- 322-coin-change/322-coin-change.java
+++ 322-coin-change/322-coin-change.java
@@ -1,33 +1,30 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
-        int m= coins.length;
-        int n=amount;
-        int [][] dp= new int[m+1][n+1];
-
-        //first row 
-        for(int j=1;j<dp[0].length;j++)
+        int m = coins.length;
+        int n = amount;
+        int [][] dp = new int[m+1][n+1];
+        
+        for(int j = 1; j < dp[0].length; j++)
         {
-            dp[0][j]=99999;
+            dp[0][j] = 99999;
         }
-
-        for(int i=1;i<dp.length;i++)
+        
+        for(int i = 1; i < dp.length; i++)
         {
-            for(int j=1;j<dp[0].length;j++)
+            for(int j = 1; j < dp[0].length; j++)
             {
-                if(j<coins[i-1])
+                if(j < coins[i-1])
                 {
-                    dp[i][j]=dp[i-1][j];
+                    dp[i][j] = dp[i-1][j];
                 }
                 else
                 {
-                    dp[i][j]=Math.min(dp[i-1][j], dp[i][j-coins[i-1]] +1);
+                    dp[i][j] = Math.min(dp[i-1][j], dp[i][j-coins[i-1]] + 1);
                 }
             }
-        }
-
-        if(dp[m][n]>=99999) return -1;
+        }                                                            
+    
+        if(dp[m][n] >= 99999) return -1;
         return dp[m][n];
-
-
-    }
+    }  
 }
