0762d2ad66e4c8da89c078d8b0a623006be4c566
Shivani Chavan <76966366+Shivani31996@users.noreply.github.com>
2022-06-12 12:39:18 -0400

    Time: 1 ms (35.38%), Space: 41.4 MB (73.32%) - LeetHub

--- 75-sort-colors/75-sort-colors.java
+++ 75-sort-colors/75-sort-colors.java
@@ -5,25 +5,25 @@ class Solution {
             return;
         }
         
-        int start = 0;
-        int end = nums.length - 1;
+        int low = 0;
+        int high = nums.length - 1;
         int index = 0;
         
-        while(start < end && index <= end)
+        while(low < high && index <= high)
         {
             if(nums[index] == 0)
             {
-                nums[index] = nums[start];
-                nums[start] = 0;
-                start++;
+                nums[index] = nums[low];
+                nums[low] = 0;
+                low++;
                 index++;                
             }
             
             else if(nums[index] == 2)
             {
-                nums[index] = nums[end];
-                nums[end] = 2;
-                end--;
+                nums[index] = nums[high];
+                nums[high] = 2;
+                high--;
             }
             
             else{
