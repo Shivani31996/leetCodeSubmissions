0da00fc9b54bc9b0f4391193eeeb26c50c9a1257
Shivani Chavan <76966366+Shivani31996@users.noreply.github.com>
2022-10-19 19:51:08 -0400

    Time: 20 ms (9.27%), Space: 55.6 MB (80.94%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,46 +1,50 @@
 class Solution {
-    int [][]dirs;
-    
+    int [][] dirs;
     public int numIslands(char[][] grid) {
-        if(grid == null || grid.length == 0)
+        
+        if(grid.length == 0 || grid == null)
         {
             return 0;
         }
         
-        dirs = new int[][]{{1,0},{0,1},{-1,0},{0,-1}};
+        dirs = new int[][] {{0,1},{-1,0},{0,-1},{1,0}};
         int m = grid.length;
         int n = grid[0].length;
         int count = 0;
         
+        Queue<Integer> qr = new LinkedList<>();
+        Queue<Integer> qc = new LinkedList<>();
+        
         for(int i = 0; i<m;i++)
         {
-            for(int j = 0;j<n;j++)
+            for(int j = 0; j<n;j++)
             {
                 if(grid[i][j] == '1')
                 {
                     count++;
-                    dfs(grid,i,j,m,n);
+                    qr.add(i);
+                    qc.add(j);
+                    grid[i][j] = '0';
+                    while(!qr.isEmpty()){
+                        int cr = qr.poll();
+                        int cc = qc.poll();
+                        
+                        for(int dir[]: dirs)
+                        {
+                            int nr = cr + dir[0];
+                            int nc = cc + dir[1];
+                            
+                            if(nr >= 0 && nc >= 0 && nr < m && nc < n && grid[nr][nc] == '1')
+                            {
+                                qr.add(nr);
+                                qc.add(nc);
+                                grid[nr][nc] = '0';
+                            }
+                        }
+                    }
                 }
             }
         }
         return count;
-    }
-    
-    private void dfs(char[][] grid, int i, int j,int m,int n)
-    {
-        //base
-        if(i < 0 || j < 0 || i == m || j == n || grid[i][j] == '0')
-        {
-            return;
-        }
-        //logic
-        grid[i][j] = '0';
-        for(int[] dir:dirs)
-        {
-            int nr = dir[0] + i;
-            int nc = dir[1] + j;
-            
-            dfs(grid,nr,nc,m,n);
-        }
     }
 }
