38d37029365d98cecc9e1157b33a8a19a1960467
Shivani Chavan <76966366+Shivani31996@users.noreply.github.com>
2022-10-27 22:49:42 -0400

    Time: 22 ms (21.28%), Space: 43.4 MB (18.08%) - LeetHub

---
+++ 0273-integer-to-english-words/0273-integer-to-english-words.java
@@ -0,0 +1,45 @@
+class Solution {
+    String[] below_20 = {"", "One ", "Two ", "Three ", "Four ", "Five ","Six ", "Seven ",
+                            "Eight ", "Nine ","Ten ","Eleven ","Twelve ","Thirteen ",
+                            "Fourteen ","Fifteen ","Sixteen ","Seventeen ","Eighteen ",
+                            "Nineteen "};
+    String[] tens = {"","Ten ","Twenty ","Thirty ","Forty ","Fifty ","Sixty ",
+                        "Seventy ","Eighty ","Ninety "};
+    String[] thousands = {"","Thousand ","Million ","Billion "};
+    
+    public String numberToWords(int num) {   
+        int i = 0;
+        String result = "";
+        
+        if(num == 0)
+            return "Zero";
+        
+        while(num > 0)
+        {
+            int curr = num%1000;
+            if(curr != 0)
+            {
+                result = helper(curr) + thousands[i] + result;
+            }
+            i++;
+            num = num/1000;
+        }
+        return result.trim();
+    }
+    
+    private String helper(int curr)
+    {
+        if(curr == 0)
+            return "";
+        else if(curr < 20)
+            return below_20[curr];
+        else if(curr < 100)
+        {
+            return tens[curr/10] + helper(curr%10);
+        }
+        else
+        {
+            return below_20[curr/100] + "Hundred " + helper(curr%100);
+        }
+    }
+}
