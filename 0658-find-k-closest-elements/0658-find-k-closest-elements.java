78dace40796499902ddf36a1fcf2a65f19a56b68
Shivani Chavan <76966366+Shivani31996@users.noreply.github.com>
2022-11-11 12:13:41 -0500

    Time: 103 ms (7.45%), Space: 68.8 MB (7.25%) - LeetHub

---
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.java
@@ -0,0 +1,30 @@
+class Solution {
+    public List<Integer> findClosestElements(int[] arr, int k, int x) {
+        PriorityQueue<int []> pq = new PriorityQueue<>((a,b) ->
+            {
+               if(a[1] == b[1])
+               {
+                   return b[0] - a[0];
+               }
+                return b[1] - a[1];
+            });
+        
+        for(int i = 0; i<arr.length;i++)
+        {
+            pq.add(new int[]{arr[i],Math.abs(arr[i] - x)});
+            if(pq.size() > k)
+            {
+                pq.poll();
+            }
+        }
+        
+        List<Integer> result = new ArrayList<>();
+        while(!pq.isEmpty())
+        {
+            int[] pair = pq.poll();
+            result.add(pair[0]);
+        }
+        Collections.sort(result);
+        return result;
+    }
+}
