e845c185e957a84ea45e0d23d84c3ee64c548f8a
Shivani Chavan <76966366+Shivani31996@users.noreply.github.com>
2022-11-19 12:19:31 -0500

    Time: 14 ms (86.47%), Space: 44 MB (67.27%) - LeetHub

--- 0227-basic-calculator-ii/0227-basic-calculator-ii.java
+++ 0227-basic-calculator-ii/0227-basic-calculator-ii.java
@@ -1,36 +1,44 @@
 class Solution {
     public int calculate(String s) {
-        Stack<Integer> st = new Stack<>();
+        int num = 0;
+        int calc = 0;
+        int tail = 0;
         
-        int currNum = 0;
         int lastSign = '+';
-        int result = 0;
         
-        for(int i = 0; i<s.length();i++)
+        for(int i = 0; i < s.length();i++)
         {
             char c = s.charAt(i);
             if(Character.isDigit(c))
             {
-                currNum = 10*currNum + c - '0';
+                num = num*10 + c - '0';
             }
-            if((!Character.isDigit(c) && c != ' ') || i == s.length() - 1)
+            if((!Character.isDigit(c) && c != ' ') || i == s.length() -1 )
             {
                 if(lastSign == '+')
-                    st.push(currNum);
+                {
+                    calc = calc + num;
+                    tail = num;
+                }
                 else if(lastSign == '-')
-                    st.push(-currNum);
+                {
+                    calc = calc - num;
+                    tail = -num;
+                }
                 else if(lastSign == '*')
-                    st.push(st.pop()*currNum);
+                {
+                    calc = calc - tail + tail * num;
+                    tail = num * tail;
+                }
                 else if(lastSign == '/')
-                    st.push(st.pop()/currNum);
-                currNum = 0;
+                {
+                    calc = calc - tail + tail/num;
+                    tail = tail/num;
+                }
+                num = 0;
                 lastSign = c;
             }
         }
-        while(!st.isEmpty())
-        {
-            result += st.pop();
-        }
-        return result;
+        return calc;
     }
 }
