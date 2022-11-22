a4160177b18392655faa7098c04b560671a4dc39
Shivani Chavan <76966366+Shivani31996@users.noreply.github.com>
2022-11-22 18:09:39 -0500

    Time: 0 ms (100.00%), Space: 42.5 MB (90.68%) - LeetHub

--- 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.java
+++ 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.java
@@ -24,29 +24,24 @@ class Node {
 class Solution {
     public Node connect(Node root) {
         if(root == null)
-        {
-            return null;
-        }
-        
-        Node level = root;
-        
-        while(level.left != null)
-        {
-            Node curr = level;
-            
-            while(curr != null)
-            {
-                curr.left.next = curr.right;
-                
-                if(curr.next != null)
-                {
-                    curr.right.next = curr.next.left;
-                }
-                curr = curr.next;
-            }
-            level = level.left;
-
-        }
+            return root;
+        dfs(root);
         return root;
+    }
+    public void dfs(Node root)
+    {
+        //base
+        if(root.left == null)
+        {
+            return;
+        }
+        //logic
+        root.left.next = root.right;
+        if(root.next != null)
+        {
+            root.right.next = root.next.left;
+        }
+        dfs(root.left);
+        dfs(root.right);
     }
 }
