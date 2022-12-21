2d9100a9a307a06fbbb176d96c072f49eaf52580
Shivani Chavan <76966366+Shivani31996@users.noreply.github.com>
2022-12-21 15:42:17 -0500

    Time: 13 ms (31.48%), Space: 42.7 MB (75.95%) - LeetHub

--- 0120-triangle/0120-triangle.java
+++ 0120-triangle/0120-triangle.java
@@ -1,20 +1,23 @@
 class Solution {
     public int minimumTotal(List<List<Integer>> triangle) {
         int n = triangle.size();
-        int[] dp = new int[n];
+        int[][] path = new int[n][n];
         
-        for(int j = 0; j < n; j++)
-        {
-            dp[j] = triangle.get(n-1).get(j);
-        }
-        
-        for(int i = n-2; i>=0; i--)
+        for(int i=n-2; i>=0; i--)
         {
             for(int j=0; j<triangle.get(i).size(); j++)
             {
-                dp[j] = triangle.get(i).get(j) + Math.min(dp[j],dp[j+1]);
+                int min = triangle.get(i+1).get(j);
+                path[i][j] = j;
+                if(triangle.get(i+1).get(j) > triangle.get(i+1).get(j+1))
+                {
+                    min = triangle.get(i+1).get(j+1);
+                    path[i][j] = j+1;
+                }
+                triangle.get(i).set(j,triangle.get(i).get(j) + min);
             }
         }
-        return dp[0];
+        System.out.println(Arrays.deepToString(path));
+        return triangle.get(0).get(0);
     }
 }
