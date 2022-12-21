83193d268c82ad325f091e3f768efc0aee5e49d9
Shivani Chavan <76966366+Shivani31996@users.noreply.github.com>
2022-12-21 17:52:49 -0500

    Time: 1 ms (99.85%), Space: 43 MB (78.34%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int jump(int[] nums) {
+        int jumps = 1;
+        int currInt = nums[0];
+        int nextInt = nums[0];
+        
+        if(nums.length == 1)
+            return 0;
+        
+        for(int i=1; i < nums.length;i++)
+        {
+            nextInt = Math.max(nextInt,nums[i] + i);
+            if(i == currInt && i != nums.length - 1)
+            {
+                jumps++;
+                currInt = nextInt;
+            }
+        }
+        return jumps;
+    }
+}
