b514bb16917993c045a8f6fa6feab34ea942d7ab
Shivani Chavan <76966366+Shivani31996@users.noreply.github.com>
2022-12-29 15:41:49 -0500

    Time: 1 ms (100.00%), Space: 42 MB (96.52%) - LeetHub

---
+++ 0136-single-number/0136-single-number.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        int bitmask = 0;
+        
+        for(int num: nums)
+        {
+            bitmask ^= num;
+        }
+        
+        return bitmask;
+    }
+}
