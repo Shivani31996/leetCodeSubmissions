f32dcd47bc0bef81e442fd3e4dfd6ea2dae6f8f9
Shivani Chavan <shivani.chavan96@gmail.com>
2023-07-23 22:00:07 -0400

    Time: 0 ms (100.00%), Space: 40 MB (91.27%) - LeetHub

--- 0993-cousins-in-binary-tree/0993-cousins-in-binary-tree.java
+++ 0993-cousins-in-binary-tree/0993-cousins-in-binary-tree.java
@@ -14,42 +14,49 @@
  * }
  */
 class Solution {
-    TreeNode x_parent;
-    TreeNode y_parent;
-    int x_level;
-    int y_level;
-    
     public boolean isCousins(TreeNode root, int x, int y) {
-        if(root == null)
-            return false;
         
-        dfs(root,x,y,null,0);
-        if(x_parent != y_parent && x_level == y_level)
-            return true;
-        else
-            return false;
-    }
-    
-    private void dfs(TreeNode root, int x, int y,TreeNode parent, int level)
-    {
-        //base
+        Queue<TreeNode> q = new LinkedList<>();
         if(root == null)
-            return;
-        
-        //logic
-        if(root.val == x)
         {
-            x_parent = parent;
-            x_level = level;
+            return false;
+        }
+        q.add(root);
+        while(!q.isEmpty())
+        {
+            int size = q.size();
+            boolean x_flag = false;
+            boolean y_flag = false;
+            
+            for(int i = 0; i < size; i++)
+            {
+                TreeNode curr = q.poll();
+                if(curr.val == x)
+                    x_flag = true;
+                if(curr.val == y)
+                    y_flag = true;
+                if(curr.left != null && curr.right != null)
+                {
+                    if(curr.left.val == x && curr.right.val == y) 
+                    {
+                        return false;
+                    }
+                    if(curr.left.val == y && curr.right.val == x)
+                    {
+                        return false;
+                    }
+                }
+                if(curr.left != null)
+                    q.add(curr.left);
+                if(curr.right != null)
+                    q.add(curr.right);
+            }
+            if(x_flag && y_flag)
+                return true;
+            if(x_flag || y_flag)
+                return false;
         }
         
-        if(root.val == y)
-        {
-            y_parent = parent;
-            y_level = level;
-        }
-        
-        dfs(root.left,x,y,root,level+1);
-        dfs(root.right,x,y,root,level+1);
+        return false;
     }
 }
