4e678ae9ab1a7dd4bada08e050162daf3c86324c
Shivani Chavan <shivani.chavan96@gmail.com>
2023-08-23 13:20:02 -0400

    Time: 2 ms (87.60%), Space: 41 MB (88.09%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -1,53 +1,46 @@
 class Solution {
-    int m,n;
-    int dirs[][] = {{0,1},{0,-1},{1,0},{-1,0}};
-    Queue<int[]> q;
     public int orangesRotting(int[][] grid) {
-        if(grid == null || grid.length == 0)
-            return 0;
-        m = grid.length;
-        n = grid[0].length;
-        q = new LinkedList<>();
-            
+        int m = grid.length;
+        int n = grid[0].length;
+        int time = 0;
         int fresh = 0;
-        for(int i = 0;i < m; i++)
+        Queue<int[]> q = new LinkedList<>();
+        int dirs[][] = new int[][]{{0,1},{0,-1},{1,0},{-1,0}};
+        
+        for(int i = 0; i < m; i++)
         {
-            for(int j = 0;j < n;j++)
+            for(int j = 0; j < n; j++)
             {
-                if(grid[i][j] == 1)
-                {
-                    fresh++;
-                }
-                else if(grid[i][j] == 2)
+                if(grid[i][j] == 2)
                 {
                     q.add(new int[]{i,j});
+                }
+                else if(grid[i][j] == 1)
+                {
+                    fresh++;
                 }
             }
         }
         
         if(fresh == 0)
-        {
             return 0;
-        }
         
-        int time = 0;
         while(!q.isEmpty())
         {
             int size = q.size();
-            for(int i = 0; i<size;i++)
+            
+            for(int i = 0; i<size; i++)
             {
                 int curr[] = q.poll();
-                
-                for(int dir[]:dirs)
+                for(int[] dir:dirs)
                 {
                     int nr = curr[0] + dir[0];
                     int nc = curr[1] + dir[1];
-                    
                     if(nr >= 0 && nc >= 0 && nr < m && nc < n && grid[nr][nc] == 1)
                     {
-                        grid[nr][nc] = 2;
-                        fresh--;
                         q.add(new int[]{nr,nc});
+                        fresh--;
+                        grid[nr][nc] = 2;
                     }
                 }
             }
@@ -56,7 +49,7 @@ class Solution {
         
         if(fresh != 0)
             return -1;
+
         return time - 1;
-            
     }
 }
