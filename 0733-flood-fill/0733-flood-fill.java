a8d2621a524868589777388b44c06cf2953647ad
Shivani Chavan <shivani.chavan96@gmail.com>
2023-08-26 12:04:12 -0400

    Time: 0 ms (100.00%), Space: 44 MB (59.97%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -7,28 +7,23 @@ class Solution {
         int[][] dirs = new int[][]{{0,1},{0,-1},{1,0},{-1,0}};
         
         int o_color = image[sr][sc];
-        Queue<Integer> q = new LinkedList<>();
-        q.add(sr);
-        q.add(sc);
-        image[sr][sc] = color;
-        while(!q.isEmpty())
-        {
-            int cr = q.poll();
-            int cc = q.poll();
-            
-            for(int[] dir: dirs)
-            {
-                int nr = cr + dir[0];
-                int nc = cc + dir[1];
-            
-                if(nr >=0 && nc >= 0 && nr < m && nc < n && image[nr][nc] == o_color)
-                {
-                    image[nr][nc] = color;
-                    q.add(nr);
-                    q.add(nc);
-                }
-            }
-        }
+        dfs(image,sr,sc,color,o_color,dirs,m,n);
         return image;
+    }
+    
+    private void dfs(int[][] image, int sr, int sc, int color, int o_color, int[][]dirs,int m, int n)
+    {
+        //base
+        if(sr < 0 || sc < 0 || sr == m || sc == n || image[sr][sc] != o_color)
+            return;
+        
+        //logic
+        image[sr][sc] = color;
+        for(int[] dir: dirs)
+        {
+            int nr = sr + dir[0];
+            int nc = sc + dir[1];
+            dfs(image,nr,nc,color,o_color,dirs,m,n);
+        }
     }
 }
