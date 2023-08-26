0f73752acd1ac8e105c0cdae3f29ff9476a29052
Shivani Chavan <shivani.chavan96@gmail.com>
2023-08-26 13:31:29 -0400

    Time: 11 ms (81.08%), Space: 45.2 MB (81.88%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.java
+++ 0542-01-matrix/0542-01-matrix.java
@@ -1,53 +1,48 @@
 class Solution {
     public int[][] updateMatrix(int[][] matrix) {
-        if(matrix.length == 0 || matrix == null)
-        {
-            return matrix;
-        }
-        
-        Queue<int []> q = new LinkedList<>();
-        
         int m = matrix.length;
         int n = matrix[0].length;
+        int dirs[][] = new int[][]{{0,1},{0,-1},{1,0},{-1,0}};
+        Queue<int []> q= new LinkedList<>();
         
-        int dirs[][] = {{0,1},{0,-1},{1,0},{-1,0}};
-        
-        for (int i = 0; i < m;i++)
+        for(int i = 0; i < m; i++)
         {
-            for(int j = 0;j<n;j++)
+            for(int j = 0; j < n; j++)
             {
                 if(matrix[i][j] == 0)
                 {
                     q.add(new int[]{i,j});
                 }
-                else 
+                else if(matrix[i][j] == 1)
                 {
                     matrix[i][j] = -1;
                 }
             }
         }
         
-        int dist = 1;
+        int distance = 0;
+        
         while(!q.isEmpty())
         {
             int size = q.size();
-            for(int i = 0; i < size;i++)
+            for(int i = 0; i < size; i++)
             {
-                int curr[] = q.poll();
-                for(int dir[]:dirs)
+                int[] curr = q.poll();
+                for(int[] dir: dirs)
                 {
                     int nr = dir[0] + curr[0];
                     int nc = dir[1] + curr[1];
-                    
                     if(nr >= 0 && nc >= 0 && nr < m && nc < n && matrix[nr][nc] == -1)
                     {
-                        matrix[nr][nc] = dist;
-                        q.add(new int[]{nr,nc});
+                    matrix[nr][nc] = distance + 1;
+                    q.add(new int[] {nr,nc});
                     }
+
                 }
             }
-            dist++;
+            distance++;
         }
+        
         return matrix;
     }
 }
