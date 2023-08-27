ed9f3a7e4fbd9c43fa20001d82b090320b8f1d3f
Shivani Chavan <shivani.chavan96@gmail.com>
2023-08-27 13:18:15 -0400

    Time: 11 ms (8.89%), Space: 50.2 MB (22.30%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -4,6 +4,8 @@ class Solution {
         int n = grid[0].length;
         int[][] dirs = new int[][]{{0,1},{0,-1},{1,0},{-1,0}};
         int count = 0;
+        Queue<Integer> rq = new LinkedList<>();
+        Queue<Integer> cq = new LinkedList<>();
         
         for(int i = 0; i < m; i++)
         {
@@ -12,25 +14,29 @@ class Solution {
                 if(grid[i][j] == '1')
                 {
                     count++;
-                    dfs(grid,i,j,m,n,dirs);
+                    rq.add(i);
+                    cq.add(j);
+                    grid[i][j] = '0';
+                    while(!rq.isEmpty() && !cq.isEmpty())
+                    {
+                        int curr_r = rq.poll();
+                        int curr_c = cq.poll();
+                        
+                        for(int[] dir: dirs)
+                        {
+                            int nr = curr_r + dir[0];
+                            int nc = curr_c + dir[1];
+                            if(nr >= 0 && nc >= 0 && nr < m && nc < n && grid[nr][nc] == '1')
+                            {
+                                rq.add(nr);
+                                cq.add(nc);
+                                grid[nr][nc] = '0';
+                            }
+                        }
+                    }
                 }
             }
         }
         return count;
-    }
-    
-    private void dfs(char[][] grid, int i, int j, int m, int n, int[][] dirs)
-    {
-        //base
-        if(i < 0 || j < 0 || i == m || j == n || grid[i][j] == '0')
-            return;
-        //logic
-        for(int[] dir: dirs)
-        {
-            grid[i][j] = '0';
-            int nr = dir[0] + i;
-            int nc = dir[1] + j;
-            dfs(grid,nr,nc,m,n,dirs);
-        }
     }
 }
