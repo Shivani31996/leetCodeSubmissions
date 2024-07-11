d87bfe3486b739e8aa6fc41e37b97778ab51a69a
Shivani Chavan <shivani.chavan96@gmail.com>
2024-07-11 18:56:45 -0400

    Time: 2 ms (85.36%), Space: 44.6 MB (27.76%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,11 +1,9 @@
 class Solution {
-    
     List<List<Integer>> result;
-    
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         result = new ArrayList<>();
         
-        if(candidates.length == 0 || candidates == null)
+        if(candidates == null || candidates.length == 0)
         {
             return result;
         }
@@ -14,25 +12,29 @@ class Solution {
         return result;
     }
     
-    private void helper(int[] candidates, int target, int index, List<Integer> path)
+    private void helper(int[] candidates, int target, int i, List<Integer> path)
     {
         //base
-        if(target < 0)
-            return;
-        
         if(target == 0)
         {
             result.add(new ArrayList<>(path));
-            return; }
+            return;
+        }
+        if(i == candidates.length || target < 0)
+            return;
         
         //logic
-        for(int i = index; i< candidates.length;i++)
-        {
-            path.add(candidates[i]);
-            
-            helper(candidates,target - candidates[i],i,path);
-            
-            path.remove(path.size() - 1);
-        }
+        
+        //choose
+        
+        //action
+        path.add(candidates[i]);
+        //recurse
+        helper(candidates,target - candidates[i], i, path);
+        //backtrack
+        path.remove(path.size() - 1);
+        
+        //no choose
+        helper(candidates,target,i+1,path);
     }
 }
