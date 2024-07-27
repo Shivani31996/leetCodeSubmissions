95134c376efaca5cea853cfcb33b9cba60906514
Shivani Chavan <shivani.chavan96@gmail.com>
2024-07-27 18:39:13 -0400

    Time: 1 ms (99.73%), Space: 58.3 MB (9.12%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.java
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.java
@@ -1,49 +1,25 @@
 class Solution {
     public int minSubArrayLen(int target, int[] nums) {
-        if(nums.length == 0 || nums == null)
-            return 0;
+        int i = 0;
+        int j = 0;
+        int currSum = 0;
+        int len = Integer.MAX_VALUE;
         
-        int low = 0;
-        int high = nums.length - 1;
-        int result = 0;
-        
-        while(low <= high)
+        while(j < nums.length)
         {
-            int mid = low + (high - low)/2;
+            currSum += nums[j];
+            j++;
             
-            boolean window = checkWindow(nums,target,mid);
-            if(window == true)
+            while(currSum >= target)
             {
-                high = mid - 1;
-                result = mid + 1;
-            }
-            else
-                low = mid + 1;
-        }
-        return result;
-    }
-    
-    private boolean checkWindow(int[] nums, int target, int mid)
-    {
-        int i = 0; int sum = 0;int j = 0;
-        for(i = 0; i <= mid; i++)
-        {
-            sum += nums[i];
-            if(sum >= target)
-            {
-                return true;
+                int currSize = j - i;
+                len = Math.min(len, currSize);
+                
+                currSum -= nums[i];
+                i++;
             }
         }
         
-        while(i <= nums.length - 1)
-        {
-            sum += nums[i++] - nums[j++];
-            if(sum >= target)
-            {
-                return true;
-            }
-        }
-        
-        return false;
+        return len == Integer.MAX_VALUE?0:len;
     }
 }
