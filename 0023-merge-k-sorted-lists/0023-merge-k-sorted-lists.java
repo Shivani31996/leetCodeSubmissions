5240a410493eb44413d08f3f865eb99756bc535e
Shivani Chavan <shivani.chavan96@gmail.com>
2024-08-02 22:54:05 -0400

    Time: 4 ms (74.70%), Space: 44.7 MB (20.15%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -10,40 +10,35 @@
  */
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
-        ListNode merged = new ListNode(Integer.MIN_VALUE);
-        for(ListNode li:lists)
+        if(lists == null || lists.length == 0)
         {
-            merged = merge(li,merged);
+            return null;
         }
-        return merged.next;
-    }
-    
-    public ListNode merge(ListNode h1, ListNode h2)
-    {
+        
+        PriorityQueue<ListNode> pq = new PriorityQueue<>((a,b) -> a.val - b.val);
+        
+        for(ListNode list: lists)
+        {
+            if(list != null)
+            {
+                pq.add(list);
+            }
+            
+        }
+        
         ListNode result = new ListNode(-1);
         ListNode curr = result;
-        while(h1 != null && h2 != null)
-        {   
-            if(h1.val < h2.val)
-            {
-                curr.next = h1;
-                h1 = h1.next;
-            }
-            else
-            {
-                curr.next = h2;
-                h2 = h2.next;
-            }
+        while(!pq.isEmpty())
+        {
+            ListNode min = pq.poll();
+            curr.next = min;
             curr = curr.next;
+            if(min.next != null)
+            {
+                pq.add(min.next);
+            }
         }
-            if(h1 != null)
-            {
-                curr.next = h1;
-            }
-            if(h2 != null)
-            {
-                curr.next = h2;
-            }
+        
         return result.next;
     }
 }
