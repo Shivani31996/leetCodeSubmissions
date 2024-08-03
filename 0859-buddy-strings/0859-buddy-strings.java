e44bf582f4960037c0149b97255aca404e111c2e
Shivani Chavan <shivani.chavan96@gmail.com>
2024-08-03 19:04:03 -0400

    Time: 5 ms (32.43%), Space: 42.4 MB (53.73%) - LeetHub

--- 0859-buddy-strings/0859-buddy-strings.java
+++ 0859-buddy-strings/0859-buddy-strings.java
@@ -1,49 +1,57 @@
 class Solution {
     public boolean buddyStrings(String s, String goal) {
-        if(s.length()!=goal.length())
-        {
+        if(s.length() != goal.length())
             return false;
-        }
-        int count=0;
-        for(int i=0;i<s.length();i++)
+        int count = 0;
+        int s_arr[] = new int[26];
+        int goal_arr[] = new int[26];
+        
+        HashSet<Character> set = new HashSet<>();
+        
+        for(int i = 0; i < s.length(); i++)
         {
-            if(s.charAt(i)!=goal.charAt(i))
+            if(s.charAt(i) != goal.charAt(i))
             {
                 count++;
             }
         }
-        int a[]=new int[26];
-        int b[]=new int[26];
-        for(char c:s.toCharArray())
+        
+        if(s.equals(goal))
         {
-            a[c-'a']++;
+            for(int i = 0; i < s.length(); i++)
+            {
+                if(!set.add(s.charAt(i)))
+                {
+                    return true;
+                }
+            }
+            return false;
         }
-        for(char c:goal.toCharArray())
-        {
-            b[c-'a']++;
-        }
-        if(count>2)
+        
+        if(count > 2)
         {
             return false;
         }
-        for(int i=0;i<26;i++)
+        
+        for(int i = 0; i < s.length(); i++)
         {
-            if(a[i]!=b[i])
+            s_arr[s.charAt(i) - 'a']++;
+            goal_arr[goal.charAt(i) - 'a']++;
+        }
+        
+        for(int i = 0; i< s_arr.length; i++)
+        {
+            if(s_arr[i] != goal_arr[i])
             {
                 return false;
             }
         }
-        if(count==2)
+        
+        if(count == 2)
         {
             return true;
         }
-        for(int i=0;i<26;i++)
-        {
-            if(a[i]>=2)
-            {
-                return true;
-            }
-        }
+        
         return false;
     }
 }
