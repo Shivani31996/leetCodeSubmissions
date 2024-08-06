ebc440dfc26291dfa87961c54e271a5a479d9862
Shivani Chavan <shivani.chavan96@gmail.com>
2024-08-06 19:57:01 -0400

    Time: 0 ms (100.00%), Space: 40.5 MB (15.41%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -3,16 +3,16 @@ class Solution {
         if(n == 0 || n == 1)
             return 1;
         
-        int[] dp = new int[n+1];
-        Arrays.fill(dp,-1);
-        dp[0] = 1;
-        dp[1] = 1;
+        int prev2 = 1;
+        int prev = 1;
         
         for(int i = 2; i <= n; i++)
         {
-            dp[i] = dp[i - 1] + dp[i - 2];
+            int curr_i = prev + prev2;
+            prev2 = prev;
+            prev = curr_i;
         }
         
-        return dp[n];
+        return prev;
     }
 }
