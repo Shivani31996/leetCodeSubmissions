b821bf038548418a007d689a1619f22413f8dd71
Shivani Chavan <shivani.chavan96@gmail.com>
2024-08-10 20:08:11 -0400

    Time: 1 ms (94.06%), Space: 44.3 MB (78.89%) - LeetHub

---
+++ 0046-permutations/0046-permutations.java
@@ -0,0 +1,33 @@
+class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        List<Integer> path = new ArrayList<>();
+        boolean map[] = new boolean[nums.length];
+        
+        helper(nums,result,path,map);
+        return result;
+    }
+    
+    private void helper(int[] nums, List<List<Integer>> result, List<Integer> path, boolean map[])
+    {
+        //base
+        if(path.size() == nums.length)
+        {
+            result.add(new ArrayList<>(path));
+            return;
+        }
+        
+        //logic
+        for(int i = 0; i < nums.length; i++)
+        {
+            if(map[i] != true)
+            {
+                path.add(nums[i]);
+                map[i] = true;
+                helper(nums,result,path,map);
+                path.remove(path.size() - 1);
+                map[i] = false;
+            }
+        }
+    }
+}
