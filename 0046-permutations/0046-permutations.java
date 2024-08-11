9a5f0de7532eaabeea82ad8e3e0a616193f5620c
Shivani Chavan <shivani.chavan96@gmail.com>
2024-08-10 21:22:03 -0400

    Time: 1 ms (94.06%), Space: 44.4 MB (45.12%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -3,7 +3,7 @@ class Solution {
         List<List<Integer>> result = new ArrayList<>();
         List<Integer> path = new ArrayList<>();
         boolean map[] = new boolean[nums.length];
-        
+        Arrays.sort(nums);
         helper(nums,result,path,map);
         return result;
     }
