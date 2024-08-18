06fad00e9c38b06b2ba5cd41f55d2ac6f9916512
Shivani Chavan <shivani.chavan96@gmail.com>
2024-08-18 18:30:16 -0400

    Time: 16 ms (22.72%), Space: 48.8 MB (31.05%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -2,13 +2,17 @@ class Solution {
     public int[] topKFrequent(int[] nums, int k) {
         HashMap<Integer,Integer> map = new HashMap<>();
         
+        //maintain a key-value pair in HashMap for all nums
         for(int num: nums)
         {
             map.put(num,map.getOrDefault(num,0)+1);
         }
         
+        //Add the keys in HashMap to a PQ/minHeap which will order the elements based on the 
+        //freq of it
         PriorityQueue<Integer> pq = new PriorityQueue<>((a,b) -> map.get(a) - map.get(b));
         
+        //Iterate over the keys in HashMap and enter it in PQ
         for(int key: map.keySet())
         {
             pq.add(key);
@@ -18,6 +22,8 @@ class Solution {
             }
         }
         
+        //In the end, only k elements remain in the PQ which is our answer which
+        //we write to the result array
         int result[] = new int[k];
         for(int i = 0; i<k; i++)
         {
