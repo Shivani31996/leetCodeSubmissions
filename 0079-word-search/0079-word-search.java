009825196c2d81fd9ac8c8c2dda8d634f35550c7
Shivani Chavan <shivani.chavan96@gmail.com>
2024-09-30 21:53:17 -0400

    Time: 231 ms (17.62%), Space: 41.5 MB (65.38%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,53 +1,48 @@
 class Solution {
-    
-    private int [][] dirs;
     int m;
     int n;
+    int[][] dirs;
     
-    public boolean exist(char[][] board, String word) {
-        if(board == null || board.length == 0)
-            return false;
-        
-        dirs = new int[][]{{0,1},{1,0},{0,-1},{-1,0}};
+public boolean exist(char[][] board, String word) {
         m = board.length;
         n = board[0].length;
-        
-        for(int i = 0; i < m; i++)
-        {
-            for(int j = 0; j < n; j++)
-            {
-                if(word.charAt(0) == board[i][j])
-                {
-                    if(backtrack(board,word,0,i,j))
+        dirs = new int[][]{{0,1},{0,-1},{1,0},{-1,0}};
+
+        for(int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == word.charAt(0)) {
+                    if(helper(board, word, 0, i, j))
                         return true;
                 }
             }
         }
         return false;
     }
-    
-    private boolean backtrack(char[][] board, String word, int index, int i,int j)
+
+    private boolean helper(char[][] board, String word,int index,int i, int j)
     {
         //base
         if(index == word.length())
+        {
             return true;
-        
+        }
+
         if(i < 0 || j < 0 || i == m || j == n || board[i][j] == '#')
             return false;
-        
+
         //logic
         if(board[i][j] == word.charAt(index))
         {
-        for(int dir[]:dirs)
+            for (int dir[]: dirs)
             {
-                int r = i + dir[0];
-                int c = j + dir[1];
-                //action
+                int nr = dir[0] + i;
+                int nc = dir[1] + j;
+
                 board[i][j] = '#';
-                //recurse
-                if(backtrack(board,word,index + 1,r,c))
+
+                if(helper(board,word,index+1,nr,nc))
                     return true;
-                //backtrack
+
                 board[i][j] = word.charAt(index);
             }
         }
