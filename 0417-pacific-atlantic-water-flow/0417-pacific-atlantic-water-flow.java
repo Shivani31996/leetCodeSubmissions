af22b2d4d8ba4d87ea9e1372730ae18d180a326e
Shivani Chavan <shivani.chavan96@gmail.com>
2025-04-21 14:50:53 -0400

    Time: 5 ms (60.97%), Space: 45.7 MB (61.88%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.java
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.java
@@ -4,17 +4,28 @@ class Solution {
         int n = heights[0].length;
         int[][] dirs = new int[][]{{0,1}, {-1,0}, {0,-1}, {1,0}};
         List<List<Integer>> result = new ArrayList<>();
+        boolean[][] pacific = new boolean[m][n];
+        boolean[][] atlantic = new boolean[m][n];
+
+        for(int i = 0; i < m; i++)
+        {
+            dfs(heights, i, 0, m, n, pacific, dirs, heights[i][0]); // pacific
+            dfs(heights, i, n-1, m, n, atlantic, dirs, heights[i][n-1]); //atlantic
+        }
+
+        for(int j = 0; j < n; j++)
+        {
+            dfs(heights, 0, j, m, n, pacific, dirs, heights[0][j]); // pacific
+            dfs(heights, m-1, j, m, n, atlantic, dirs, heights[m-1][j]); //atlantic
+        }
 
         for(int i = 0; i < m; i++)
         {
             for(int j = 0; j < n; j++)
             {
-                boolean[][] visited = new boolean[m][n];
-                boolean[] reach = new boolean[2];
-                dfs(heights, i, j, m, n, visited, dirs, heights[i][j], reach);
-                if(reach[0] && reach[1])
+                if(pacific[i][j] == true && atlantic[i][j] == true)
                 {
-                    result.add(Arrays.asList(i, j));
+                    result.add(Arrays.asList(i,j));
                 }
             }
         }
@@ -22,34 +33,31 @@ class Solution {
         return result;
     }
 
-    private void dfs(int[][] heights, int i, int j, int m, int n, boolean[][] visited, int[][] dirs, int prevHeight, boolean[] reach)
+    private void dfs(int[][] heights, int i, int j, int m, int n, boolean[][] visited, int[][] dirs, int prevHeight)
     {
         //base
-        if( i < 0 || j < 0)
+        if(i < 0 || j < 0 || i >= m || j >= n)
         {
-            reach[0] = true;
             return;
         }
 
-        if(i >= m || j >= n)
+        if(prevHeight > heights[i][j])
         {
-            reach[1] = true;
             return;
         }
 
-        if(visited[i][j] || heights[i][j] > prevHeight)
+        if(visited[i][j])
         {
             return;
         }
 
         //logic
         visited[i][j] = true;
-        for(int[] dir: dirs)
+        for(int dir[]: dirs)
         {
             int nr = dir[0] + i;
             int nc = dir[1] + j;
-
-            dfs(heights, nr, nc, m, n, visited, dirs, heights[i][j], reach);
+            dfs(heights, nr, nc, m, n, visited, dirs, heights[i][j]);
         }
     }
 }
