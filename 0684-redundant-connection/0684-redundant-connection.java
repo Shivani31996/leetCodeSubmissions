e02f7c49cc14138dea9afe9ec1d8e788d5b51fb5
Shivani Chavan <shivani.chavan96@gmail.com>
2025-04-22 06:58:47 -0400

    Time: 1 ms (72.43%), Space: 43.3 MB (50.53%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.java
+++ 0684-redundant-connection/0684-redundant-connection.java
@@ -1,56 +1,60 @@
-public class UnionFind {
+class UnionFind {
     private int[] id;
-    
-    public UnionFind(int n)
-    {
+
+    UnionFind(int n) {
         id = new int[n];
+
         for(int i = 0; i < n; i++)
         {
             id[i] = i;
         }
     }
-    
-    public void union(int u, int v)
-    {
-        int p = find(u);
-        int q = find(v);
-        
-        id[q] = p;
-    }
-    
+
     public int find(int node)
     {
         while(node != id[node])
         {
             node = id[node];
         }
+
         return node;
     }
-}
 
+    public void union(int p, int q)
+    {
+        int pRoot = find(p);
+        int qRoot = find(q);
+
+        id[qRoot] = pRoot;
+    }
+}
 class Solution {
     public int[] findRedundantConnection(int[][] edges) {
         if(edges == null || edges.length == 0)
         {
             return new int[]{};
         }
-        
+
         UnionFind uf = new UnionFind(edges.length+1);
+
         for(int[] edge: edges)
         {
-            int p = uf.find(edge[0]);
-            int q = uf.find(edge[1]);
-            
-            if(p != q)
+            int u = edge[0];
+            int v = edge[1];
+
+            int uRoot = uf.find(u);
+            int vRoot = uf.find(v);
+
+            if(uRoot != vRoot)
             {
-                uf.union(edge[0],edge[1]);
+                uf.union(u,v);
             }
             else
             {
-                return new int[]{edge[0],edge[1]};
+                return new int[]{u,v};
             }
         }
-        
+
         return new int[]{};
     }
 }
